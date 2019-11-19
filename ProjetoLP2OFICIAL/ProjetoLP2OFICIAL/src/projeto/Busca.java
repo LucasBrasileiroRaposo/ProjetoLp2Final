df22bcd9ed3d232342be0bb8b5149dd53361dbc9
Kaitx <kaio.araujo@ccc.ufcg.edu.br>
2019-11-19 10:45:00 -0300

    Tirando diretorio equivocado.

--- ProjetoLP2OFICIAL/ProjetoLP2OFICIAL/src/projeto/Busca.java
+++
@@ -1,7 +0,0 @@
-package projeto;
-
-public interface Busca{
-    String busca(String palavra);
-    int contaResultadosBusca(String termo);
-    String busca(String termo, int numeroDoResultado);
-}
